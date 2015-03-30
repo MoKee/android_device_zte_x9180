d143564bb8aa04bb74abb02c2ccd0fada4778f4c
proDOOMman <prodoomman@gmail.com>
2015-03-30 12:35:22 +0300

    Add patches to disable su icon in statusbar

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,9 +18,24 @@ sh device/ZTE/X9180/update-overlay.sh
 cd frameworks/base
 if grep -q "ro.storage_list.override" services/core/java/com/android/server/MountService.java
 then
-    echo 'Frameworks/base already patched';
+    echo '[storages] Frameworks/base already patched';
 else
     git apply ../../device/ZTE/X9180/patches/frameworks-base-1.patch;
     git apply ../../device/ZTE/X9180/patches/frameworks-base-2.patch;
+fi
+if grep -q "SHOW_SU_INDICATOR" core/java/android/provider/Settings.java
+then
+    echo '[su icon] Frameworks/base already patched';
+else
+    git apply ../../device/ZTE/X9180/patches/su-icon-frameworks-base.patch;
+fi
+croot
+
+cd packages/apps/Settings
+if grep -q "show_su_indicator" res/xml/status_bar_settings.xml
+then
+    echo '[su icon] Settings already patched';
+else
+    git apply ../../device/ZTE/X9180/patches/su-icon-settings.patch;
 fi
 croot
