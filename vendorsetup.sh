0269691db4b87605b74998e427d49b4d617e28f3
proDOOMman <prodoomman@gmail.com>
2015-04-06 17:25:13 +0300

    Abort patches, if they not applied

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,26 +20,26 @@ if grep -q "ro.storage_list.override" services/core/java/com/android/server/Moun
 then
     echo '[storages] Frameworks/base already patched';
 else
-    git am ../../device/ZTE/X9180/patches/frameworks-base-1.patch;
-    git am ../../device/ZTE/X9180/patches/frameworks-base-2.patch;
+    git am ../../device/ZTE/X9180/patches/frameworks-base-1.patch || git am --abort
+    git am ../../device/ZTE/X9180/patches/frameworks-base-2.patch || git am --abort
 fi
 if grep -q "SHOW_SU_INDICATOR" core/java/android/provider/Settings.java
 then
     echo '[su icon] Frameworks/base already patched';
 else
-    git am ../../device/ZTE/X9180/patches/su-icon-frameworks-base.patch;
+    git am ../../device/ZTE/X9180/patches/su-icon-frameworks-base.patch || git am --abort
 fi
 if grep -qs "statusbar_powerkey v01" packages/SystemUI/src/com/atx/siyang/PowerKey.java
 then
     echo '[statusbar powerkey] Frameworks/base already patched';
 else
-    git am ../../device/ZTE/X9180/patches/statusbar-powerkey-frameworks-base.patch;
+    git am ../../device/ZTE/X9180/patches/statusbar-powerkey-frameworks-base.patch || git am --abort
 fi
 if grep -q "mShowExclamationMarks" packages/SystemUI/src/com/android/systemui/statusbar/policy/MSimNetworkControllerImpl.java
 then
     echo '[signal icons] Frameworks/base already patched';
 else
-    git am ../../device/ZTE/X9180/patches/signal-without-exclamation-mark-frameworks-base.patch;
+    git am ../../device/ZTE/X9180/patches/signal-without-exclamation-mark-frameworks-base.patch || git am --abort
 fi
 croot
 
@@ -48,19 +48,19 @@ if grep -q "show_su_indicator" res/xml/status_bar_settings.xml
 then
     echo '[su icon] Settings already patched';
 else
-    git am ../../../device/ZTE/X9180/patches/su-icon-settings.patch;
+    git am ../../../device/ZTE/X9180/patches/su-icon-settings.patch || git am --abort
 fi
 if grep -q "statusbar_powerkey v01" res/xml/status_bar_settings.xml
 then
     echo '[statusbar powerkey] Settings already patched';
 else
-    git am ../../../device/ZTE/X9180/patches/statusbar-powerkey-settings.patch;
+    git am ../../../device/ZTE/X9180/patches/statusbar-powerkey-settings.patch || git am --abort
 fi
 if grep -q "exclamation marks v01" res/xml/status_bar_settings.xml
 then
     echo '[signal icons] Settings already patched';
 else 
-    git am ../../../device/ZTE/X9180/patches/signal-without-exclamation-mark-settings.patch;
+    git am ../../../device/ZTE/X9180/patches/signal-without-exclamation-mark-settings.patch || git am --abort
 fi
 croot
 
@@ -69,6 +69,6 @@ if grep -q "QCOM_MEDIA_DISABLE_BUFFER_SIZE_CHECK" mm-video-v4l2/vidc/vdec/src/om
 then
     echo '[buffer check] Media-caf mm-video-v4l2 already patched';
 else
-    git am ../../../../device/ZTE/X9180/patches/media-disable-buffer-check.patch;
+    git am ../../../../device/ZTE/X9180/patches/media-disable-buffer-check.patch || git am --abort
 fi
 croot
