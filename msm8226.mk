e2c3c2991eaf1125f8644ddcb4d6efce71cf0e52
proDOOMman <prodoomman@gmail.com>
2015-04-06 16:26:34 +0300

    Remove xhdpi hdpi from PRODUCT_AAPT_CONFIG It's enough to set it in PRODUCT_AAPT_PREF_CONFIG.

--- msm8226.mk
+++ msm8226.mk
@@ -52,7 +52,7 @@ PRODUCT_COPY_FILES += \
 -include $(LOCAL_PATH)/system_prop.mk
 
 # Screen density
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
+PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 # Boot animation
