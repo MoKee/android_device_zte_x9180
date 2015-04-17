9f7d2eca8ecec33a89aba525d175e15aa3aabbd3
proDOOMman <prodoomman@gmail.com>
2015-04-17 16:29:52 +0300

    Add power key debounce hack to keyhandler

--- keyhandler/src/com/cyanogenmod/settings/device/KeyHandler.java
+++ keyhandler/src/com/cyanogenmod/settings/device/KeyHandler.java
@@ -13,9 +13,14 @@ public class KeyHandler implements DeviceKeyHandler {
 
     // Supported scancodes
     private static final int KEY_PALM = 249;
+    private static final int KEY_POWER = 116;
 
     private final Context mContext;
     private final PowerManager mPowerManager;
+    
+    private static long power_button_timestamp = 0;
+    private static boolean action_down_consumed = false;
+    private static final int POWER_TIMESTAMP_DELTA = 400;
 
     public KeyHandler(Context context) {
         mContext = context;
@@ -31,6 +36,19 @@ public class KeyHandler implements DeviceKeyHandler {
                 mPowerManager.goToSleep(SystemClock.uptimeMillis());
             }
             consumed = true;
+            break;
+        case KEY_POWER:
+            if(event.getAction() == KeyEvent.ACTION_UP && action_down_consumed) {
+                consumed = true;
+                action_down_consumed = false;
+            } else if (event.getAction() == KeyEvent.ACTION_DOWN && action_down_consumed) {
+                long new_timestamp = System.currentTimeMillis();
+                if(new_timestamp - power_button_timestamp < POWER_TIMESTAMP_DELTA) {
+                    consumed = true;
+                    action_down_consumed = true;
+                }
+                power_button_timestamp = new_timestamp;
+            }
             break;
         }
         return consumed;
