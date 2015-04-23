0edf36679ab06a240111f44824db24752bf73b9f
martincz <martincz@mokeedev.com>
2015-04-23 20:06:42 +0800

    x9180: disable device dexopt option
    Change-Id: Idb0c13c235c8173eb9f109fcfd55e8bd4be9e517

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,14 +17,6 @@
 
 LOCAL_PATH := device/zte/x9180
 
-ifeq ($(HOST_OS),linux)
-  ifeq ($(WITH_DEXPREOPT),)
-    WITH_DEXPREOPT := true
-    WITH_DEXPREOPT_BOOT_IMG_ONLY := false
-  endif
-endif
-WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
-
 PRODUCT_COPY_FILES := $(filter-out frameworks/base/data/keyboards/Generic.kl:system/usr/keylayout/Generic.kl , $(PRODUCT_COPY_FILES))
 
 #Disable memcpy_base.S optimization
