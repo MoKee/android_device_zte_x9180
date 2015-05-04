05afe959ab34f5899869c51903749eda9989305e
proDOOMman <prodoomman@gmail.com>
2015-05-04 11:26:19 +0300

    WIP: VOIP

--- system_prop.mk
+++ system_prop.mk
@@ -33,7 +33,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.audio.ssr=false
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    use.dedicated.device.for.voip=false \
     use.voice.path.for.pcm.voip=false
 
 # Display
