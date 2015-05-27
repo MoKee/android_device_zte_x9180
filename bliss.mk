fb59593f7560a63c83d0148dde79836c750d2e59
proDOOMman <prodoomman@gmail.com>
2015-05-27 22:05:41 +0300

    BlissPop OTA

--- bliss.mk
+++ bliss.mk
@@ -1,5 +1,11 @@
 $(call inherit-product, device/ZTE/X9180/full_X9180.mk)
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.ota.romname=BlissPop-X9180 \
+    ro.ota.manifest=https://romhut.com/roms/blisspop-x9180/ota.xml
+
+BLISS_DEVICE_URL := https://romhut.com/roms/blisspop-x9180
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
