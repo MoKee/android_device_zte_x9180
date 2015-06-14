0b6eccb29a00c2d97aac9e9f0cd32d6c7475d3ab
proDOOMman <prodoomman@gmail.com>
2015-06-14 09:36:33 +0300

    Enable OTA for PAC rom

--- cm.mk
+++ cm.mk
@@ -4,15 +4,21 @@ $(call inherit-product, device/ZTE/X9180/full_X9180.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := ZTE V5 RedBull
-PRODUCT_NAME := cm_X9180
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.ota.romname=CyanogenMod-X9180 \
-    ro.ota.manifest=https://romhut.com/roms/cyanogenmod-x9180/ota.xml \
-    ro.ota.version=$(shell date +%Y%m%d)
+ifneq ($(PAC_BUILD_VERSION),)
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.ota.romname=PAC-X9180 \
+        ro.ota.manifest=https://romhut.com/roms/pac-x9180/ota.xml \
+        ro.ota.version=$(shell date +%Y%m%d)
+    PRODUCT_NAME := pac_X9180
+else
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.ota.romname=CyanogenMod-X9180 \
+        ro.ota.manifest=https://romhut.com/roms/cyanogenmod-x9180/ota.xml \
+        ro.ota.version=$(shell date +%Y%m%d)
+    PRODUCT_NAME := cm_X9180
+    PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/prebuilt/bootanimation.zip:system/media/bootanimation.zip
+endif
 
 PRODUCT_PACKAGES += OTAUpdates
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/bootanimation.zip:system/media/bootanimation.zip
-
